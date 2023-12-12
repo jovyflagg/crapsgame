739b422d9fbdf0c39e06f8bcaf9790fc485eb347
Jovy Cortez <jovycortez11@gmail.com>
2023-12-12 14:43:56 -0500

    Dice roll component added

---
+++ src/DiceRoll.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const DiceRoll = () => {
+    
+    // const sum = 0;
+    // int count = 0;
+    // while (sum != 7) {
+    //         int die1 = r.nextInt(6) + 1; // between 1 to 6 inclusive
+    //         int die2 = r.nextInt(6) + 1;
+    //     sum = die1 + die2;
+    //     System.out.println(die1 + " + " + die2 + " = " + sum);
+    //     count++;
+    // }
+    // System.out.println("sum of 7 after " + count + " rolls");
+    return (
+        <>
+            <div>DiceRoll</div>
+        {/* button for dice roll */}
+        </>
+
+    )
+}
+
+export default DiceRoll
